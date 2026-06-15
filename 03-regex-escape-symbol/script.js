f7455772836a657ad2d2ac667ae16b203c1669f3
ArturTudeng <artur.dzekunov10@gmail.com>
2026-06-15 14:50:05 +0300

    03 Lesson Files

---
+++ 03-regex-escape-symbol/script.js
@@ -0,0 +1,36 @@
+// Regex - Escape Symbol
+
+// Detect if there are any special commands in `userInput`.
+// `/help` and `/play` are the commands
+// if a user types `/help` somewhere in the text, return the message `How can I help you?`
+// if a user types `/play` somewhere in the text, return the message `Start a new game.`
+// if user types both commands in one sentence, we return the first matched command
+// otherwise return the `userInput` text
+// `userInput` is a string
+function detectCommand(userInput) {
+    const regexPattern = /\/help|\/play/g
+    const matchResult = userInput.match(regexPattern)  // [] or null
+
+    if (matchResult === null) {
+        return userInput
+    }
+
+    const specialWord = matchResult[0]
+    if (specialWord === "/help") {
+        return "How can I help you?"
+    } else {
+        return "Start a new game."
+    }
+}
+console.log(
+    detectCommand("Please help me /help."),
+)  // How can I help you?
+console.log(
+    detectCommand("I want to /play a new game."),
+)  // Start a new game.
+console.log(
+    detectCommand("Hello"),
+)  // Hello
+console.log(
+    detectCommand("/play /help"),
+)  // Start a new game.

6a9491dd5207af5c9632ba96aa36b92f529948db
ArturTudeng <artur.dzekunov10@gmail.com>
2026-06-22 18:01:02 +0300

    05 Lesson Files

---
+++ 05-regex-word-boundary/solution.js
@@ -0,0 +1,58 @@
+// Regex - Word Boundary
+
+// Count how many times exact word `cat` appears
+// do not count `cat` inside bigger words like `catapult`
+// or `cat` contains numbers or underscore like `_cat123`
+// return: number
+function countExactCat(text) {
+    const regexPattern = /\bcat\b/gi
+    const matchResult = text.match(regexPattern)
+
+    if (matchResult === null) {
+        return 0
+    } else {
+        return matchResult.length
+    }
+}
+
+console.log(
+    countExactCat("Cat and dog"),
+)  // 1
+console.log(
+    countExactCat("cat catapult my cat!"),
+)  // 2
+console.log(
+    countExactCat("_cat123, dog only")
+)  // 0
+
+
+// Count how many times exact keyword `if` appears
+// do not count `if` inside bigger words like `gift`
+// or `different`
+// return: number
+function countExactIf(text) {
+    const pattern = /\bif\b/gi
+    const matchResult = text.match(pattern)  // null or []
+
+    if (matchResult === null) {
+        return 0
+    } else {
+        return matchResult.length
+    }
+}
+
+console.log(
+    countExactIf("if(age >= 18)")
+)  // 1
+
+console.log(
+    countExactIf("gift different if")
+)  // 1
+
+console.log(
+    countExactIf("IF, if, If")
+)  // 3
+
+console.log(
+    countExactIf("gift difficult different")
+)  // 0
